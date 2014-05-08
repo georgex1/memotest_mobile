9ff63d1f0ac5da39ae031fa093fa75b5ce812ea3
georgex1 <grandgeorgex@hotmail.com>
2014-05-08 18:06:37 -0300

    init

---
+++ js/functions.js
@@ -0,0 +1,170 @@
+var windowsH = 0;
+var t = null;
+
+$(document).ready(function(){
+    windowsH = $(window).height();
+    /*$('.page_container').height(windowsH);*/
+});
+
+function change_page(page_){
+    
+    if(page_ == 'game_page'){
+        $('#info_page_bottom').animate({'left': '-2000px'}, 1000);
+        $('#info_page .container').animate({'margin-top': '-2000px'}, 1000, function() {
+            $('.page').hide();
+            $('#'+page_).show();
+            
+            $("#mesa").memotest('images/grilla/default.png', new Array(
+                    'images/grilla/1.png',
+                    'images/grilla/2.png',
+                    'images/grilla/3.png',
+                    'images/grilla/4.png',
+                    'images/grilla/5.png',
+                    'images/grilla/6.png'
+            ));
+            
+        });
+    }else if(page_ == 'home'){
+        location.reload(); 
+        stopSnow();
+        
+        $('#info_page_bottom').css('left', '0px');
+        $('#info_page .container').css('margin-top', '0px');
+        
+        $('.page').fadeOut('500', function(){ $('#'+page_).fadeIn();});
+        
+        var sound_win = document.getElementById('sound_win');
+        sound_win.pause();
+        sound_win.currentTime = 0;
+        
+    }else if(page_ == 'win_page'){
+        var sound_win = document.getElementById('sound_win');
+        sound_win.play();
+        
+        $('.page').fadeOut('500', function(){ 
+            $('#'+page_).show();
+            showSnow();
+            
+            $('#win_page .container_left').animate({'margin-left': '8%'}, 1000);
+            $('#win_page .container_right').animate({'margin-top': '0px'}, 1000);
+            
+        });
+        
+    }else if(page_ == 'loose_page'){
+        var sound_game_over = document.getElementById('sound_game_over');
+        sound_game_over.play();
+        
+        $('.page').fadeOut('500', function(){
+            
+            $('#'+page_).show();
+            
+            $('#loose_page .container_left').animate({'margin-left': '8%'}, 1000);
+            $('#loose_page .container_right').animate({'margin-top': '0px'}, 1000);
+            
+        });
+    } else{
+        //$('.page').hide();
+        //$('#'+page_).show();
+        $('.page').fadeOut('500', function(){ $('#'+page_).fadeIn();});
+    }
+}
+
+function changeLifes(intentos){
+    $('#vidas_cant').html(intentos);
+    $('#vidas_img img').first().remove();
+}
+
+function restartLifes(){
+    intentos = 2;
+    $('#vidas_cant').html(intentos);
+    var img_vidas = '<img src="images/vida.png" />';
+    $('#vidas_img').append(img_vidas);
+    $('#vidas_img').append(img_vidas);
+}
+
+function stopSnow(){
+    //if(t != null){
+        
+        window.setTimeout(function() {
+            $('.flake_').remove();
+            clearInterval(t);
+            t = null;
+        }, 1000);
+        //t = null;
+    //}
+}
+
+function showSnow(){
+    clearInterval(t);
+    $('body').append('<div id="flake" class="flake_"><img src="images/confetti.png" /></div>');
+t = setInterval(
+	function(){
+		var documentHeight 		= $(document).height();
+		var startPositionLeft 	= Math.random() * $(document).width() - 100;
+		var startOpacity		= 1;
+		var sizeFlake			= 10 + Math.random() * 20;
+		var endPositionTop		= documentHeight - 40;
+		var endPositionLeft		= startPositionLeft - 100 + Math.random() * 200;
+		var durationFall		= documentHeight * 10 + Math.random() * 5000;
+		$('#flake')
+			.clone()
+			.appendTo('body')
+			.css(
+				{
+					left: startPositionLeft,
+					opacity: startOpacity,
+					'font-size': sizeFlake
+				}
+			)
+			.animate(
+				{
+					top: endPositionTop,
+					left: endPositionLeft,
+					opacity: 0.8
+				},
+				durationFall,
+				'linear',
+				function() {
+					$(this).remove()
+				}
+			);
+	},500);
+
+	var snow = {};
+	var snowflex = {};
+
+	snowflex.create  = function(){
+		var flex = document.createElement('div');
+			flex.innerHTML		 	= "&#10052;";
+			flex.style.fontSize 	= 10 + Math.random() * 20 + 'px';
+			flex.style.top 			= - 50 + Math.random() * 20 + 'px';
+			flex.style.left 		= Math.random() * 1500 + 'px';
+			flex.style.position 	= "absolute";
+			flex.style.color 		= "#F3F3F3";
+			flex.style.opacity		= Math.random();
+			document.getElementsByTagName('body')[0].appendChild(flex);
+			return flex;
+	};
+
+
+	snow.snowflex = function(){
+		var flex = snowflex.create();
+		var x = -1 + Math.random() * 2;
+		t = setInterval( 
+					function(){
+						flex.style.top 	= parseInt(flex.style.top) +  5 + 'px';
+						flex.style.left = parseInt(flex.style.left) +  x + 'px';
+						if (parseInt(flex.style.top) > 1500) {
+							clearInterval(t);
+							document.getElementsByTagName('body')[0].removeChild(flex);
+						}			
+					}, 45 + Math.random() * 20);
+	};
+	
+	snow.storm = function(){
+		t	= setInterval(
+					function(){
+						snow.snowflex();
+					}, 1000);
+	};
+}
---
+++ js/memotest.js
@@ -0,0 +1,204 @@
+var intentos = 2;
+jQuery.fn.memotest = function(_carta_oculta, _cartas) {
+    var elemento = $(this);
+    this.iniciar = function() {
+	_this.carta_oculta = _carta_oculta;
+	_this.cartas = _cartas;
+        _this.jugar(true);
+    };
+    var _this = {
+        cartas: null,
+        carta_oculta: 'imagenes/00.jpg',
+        puntuaciones: new Array(),
+        jugada: 0,
+        carta1: null,
+        carta2: null,
+        time_now_inicial: null,
+        duplicarCartas: function() {
+            var cartas = new Array();
+            for (var i = 0; i < _this.cartas.length; i++) {
+                cartas.push(_this.cartas[i]);
+                cartas.push(_this.cartas[i]);
+            }
+            _this.cartas = cartas;
+        },
+        mezclarCartas: function() {
+            var contador = 0;
+            var cantidad_de_cartas = _this.cartas.length;
+            var posiciones = new Array();
+            var numero_al_azar;
+            var posicion_encontrada = false;
+            var cartas = new Array();
+            while (contador < cantidad_de_cartas) {
+                numero_al_azar = Math.floor((Math.random() * cantidad_de_cartas) + 0);
+                for (var i = 0; i < posiciones.length; i++) {
+                    if (posiciones[i] === numero_al_azar) {
+                        posicion_encontrada = true;
+                        break;
+                    }
+                }
+                if (!posicion_encontrada) {
+                    contador++;
+                    posiciones.push(numero_al_azar);
+                    cartas.push(_this.cartas[numero_al_azar]);
+                }
+                posicion_encontrada = false;
+            }
+            _this.cartas = cartas;
+        },
+        dibujarCartas: function() {
+            var codigo_html = '<ul>';
+            for (var i = 0; i < _this.cartas.length; i++) {
+                codigo_html += '<li class="memotest_cartas memotest_carta_invisible"> <img src="' + _this.carta_oculta + '" alt="" /> </li>';
+            }
+            codigo_html += '</ul>';
+            elemento.html(codigo_html);
+        },
+        mostrarCarta: function(posicion, elemento) {
+            var imagen = _this.cartas[posicion];
+            elemento.fadeOut('200', function(){
+                var sound_vuelta = document.getElementById('sound_vuelta');
+                sound_vuelta.play();
+                
+                if (_this.jugada === 0) {
+                    elemento.find('img').attr('src', _this.cartas[posicion]);
+                    elemento.removeClass('memotest_carta_invisible');
+                    _this.carta1 = elemento;
+                    _this.jugada++;
+                } else if (_this.jugada === 1) {
+                    elemento.find('img').attr('src', _this.cartas[posicion]);
+                    elemento.removeClass('memotest_carta_invisible');
+                    _this.carta2 = elemento;
+                    _this.jugada++;
+                    _this.comprobarIgualdades();
+                }
+                elemento.fadeIn('200');
+            });
+        },
+        mostrarTodasCartas: function(){
+            $('.memotest_cartas').each(function(){
+                $(this).find('img').attr('src', _this.cartas[$(this).index()]);
+                $(this).removeClass('memotest_carta_invisible');
+            });
+            
+            $('#countdown').show();
+            var cdn = 3;
+            $('#countdownNumber img').attr('src', 'images/count_'+cdn+'.png');
+            var cdInt = window.setInterval(function() {
+                cdn -= 1;
+                if(cdn > 0){
+                    
+                    $('#countdownNumber img').attr('src', 'images/count_'+cdn+'.png');
+                    //$('#countdownNumber').fadeOut('200', function(){ $('#countdownNumber').fadeOut();});
+                }
+                var sound_timer = document.getElementById('sound_timer');
+                sound_timer.pause();
+                sound_timer.currentTime = 0;
+                sound_timer.play();
+            }, 1000);
+            
+            
+            window.setTimeout(function() {
+                
+                $('.memotest_cartas').each(function(){
+                    $(this).find('img').attr('src', _this.carta_oculta);
+                    $(this).addClass('memotest_carta_invisible');
+                    clearInterval(cdInt);
+                    $('#countdown').fadeOut();
+                });
+            }, 3000);
+            //alert(_this.cartas);
+        },
+        comprobarIgualdades: function() {
+            if (_this.carta1.find('img').attr('src') === _this.carta2.find('img').attr('src')) {
+                window.setTimeout(function() {
+                    var sound_bonus = document.getElementById('sound_bonus');
+                    sound_bonus.play();
+                    
+                    _this.carta1.css('opacity', 0.8);
+                    _this.carta2.css('opacity', 0.8);
+                    _this.comprobarFinDelJuego();
+                    _this.jugada = 0;
+                }, 500);
+            } else {
+                var sound_fail = document.getElementById('sound_fail');
+                sound_fail.play();
+                intentos -=1;
+                changeLifes(intentos);
+                if(intentos < 1){
+                    window.setTimeout(function() {
+                        //change_page('loose_page');
+                        change_page('win_page');
+                        //_this.jugar(true);
+                    }, 500);
+                }
+                //if(intentos > 0){
+                    window.setTimeout(function() {
+                        _this.carta1.find('img').attr('src', _this.carta_oculta);
+                        _this.carta1.addClass('memotest_carta_invisible');
+                        _this.carta2.find('img').attr('src', _this.carta_oculta);
+                        _this.carta2.addClass('memotest_carta_invisible');
+                        _this.jugada = 0;
+                    }, 500);
+                //}
+            }
+        },
+        comprobarFinDelJuego: function() {
+            if ($(".memotest_carta_invisible").size() === 0) {
+                var milisegundos = (_this.getDateNow() - _this.time_now_inicial) / 1000;
+                var puntuacion = 1000 - milisegundos;
+                puntuacion = Math.ceil(puntuacion);
+                _this.puntuaciones.push(puntuacion);
+                elemento.html('');
+                elemento.append('<div class="memotest_fin_juego"> Fin del juego. Tu puntuaci&oacute;n es de ' + puntuacion + '.</div>');
+                _this.mostrarPuntuaciones();
+                elemento.append('<div> <a class="memotest_jugar_de_nuevo" href="javascript:void(0);"> Jugar de nuevo </a> </div>');
+            }
+        },
+        mostrarPuntuaciones: function() {
+            change_page('win_page');
+            /*var codigo_html = '<div>';
+            codigo_html += '<p> Puntuaciones: </p>';
+            codigo_html += '<ul>';
+            for (var i = 0; i < _this.puntuaciones.length; i++) {
+                codigo_html += '<li class="memotest_puntuaciones"> ' + _this.puntuaciones[i] + ' </li>';
+            }
+            codigo_html += '</ul>';
+            codigo_html += '</div>';
+            elemento.append(codigo_html);*/
+        },
+        eventos: function() {
+            elemento.delegate('.memotest_cartas', 'click', function(posicion) {
+                if ($(this).hasClass('memotest_carta_invisible')) {
+                    _this.mostrarCarta($(this).index(), $(this));
+                }
+            });
+            elemento.delegate('.memotest_jugar_de_nuevo', 'click', function() {
+                _this.jugar(false);
+            });
+        },
+        jugar: function(primera_vuelta) {
+            if (primera_vuelta) {
+                _this.duplicarCartas();
+            } else {
+                _this.jugada = 0;
+            }
+            restartLifes();
+	    _this.time_now_inicial = _this.getDateNow();
+            _this.mezclarCartas();
+            _this.dibujarCartas();
+            _this.eventos();
+            _this.mostrarTodasCartas();
+        },
+	getDateNow: function(){
+		var timeNow;
+		try{
+			timeNow = Date.now();
+		}catch(e){
+			timeNow = +new Date();
+		}
+		return timeNow;
+	}
+    };
+    return this.iniciar();
+};
